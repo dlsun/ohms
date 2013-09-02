cf654620e21da3157d5b1ec1e778c284d9aa73e9
Naftali Harris <naftaliharris@gmail.com>
2013-09-02 10:53:22 -0700

    Moved init_db outside of server

--- server.py
+++ server.py
@@ -2,10 +2,8 @@
 OHMS: Online Homework Management System
 """
 
-from base import Base, session, engine
 from flask import Flask
 app = Flask(__name__)
-import objects
 
 
 @app.route("/")
@@ -13,13 +11,5 @@ def hello():
     return "Hello World!"
 
 
-def init_db():
-    Base.metadata.create_all(engine)
-    h = objects.Homework()
-    h.from_xml('hws/example.xml')
-    session.add(h)
-    session.commit()
-
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=False)
+    app.run(debug=True)
