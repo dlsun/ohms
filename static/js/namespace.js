0e161b9238d9613c7fb436e6120268f2bfd1f0a3
Dennis Sun <dlsun@Denniss-MacBook-Pro-2.local>
2014-10-08 00:02:16 -0700

    changed mathjax defaults

--- static/js/namespace.js
+++ static/js/namespace.js
@@ -11,7 +11,7 @@ var OHMS = OHMS || {};
 
 MathJax.Hub.Config({
     tex2jax: {
-	inlineMath: [['`','`']],
+	inlineMath: [['`','`'], ['\\(','\\)']],
 	processEscapes: true,
 	skipTags: ["script","noscript","style","textarea","code"],
     },
