46e89c74f80563609d1643380873d1b18c61f15a
Dennis Sun <dlsun@Denniss-MacBook-Pro-2.local>
2015-04-21 12:02:06 -0700

    fixed bug with gradebook not loading

--- ohms/app.py
+++ ohms/app.py
@@ -323,7 +323,7 @@ def download_grades():
 
     csv += ',"MAXIMUM",,' + ",".join("" for c in categories) + "," 
     csv += ",".join(str(max_scores[hw.id]) for hw in homeworks) + "\n"
-
+    
     for student, grades in gradebook:
         row = [student.stuid, student.name, str(grades["overall"])]
         for category in categories:
@@ -334,7 +334,7 @@ def download_grades():
             else:
                 row.append("E" if grades[hw.id].excused else str(grades[hw.id].score))
         csv += ",".join(row) + "\n"
-
+        
     response = make_response(csv)
     course = options.title.replace(" ", "")
     date = datetime.now().strftime("%m-%d-%Y")
@@ -393,10 +393,12 @@ def get_gradebook():
     gradebook = gradebook.items()
     gradebook.sort(key=lambda entry: convert_to_last_name(entry[0].name))
 
+    categories = session.query(Category).all()
+    
     # calculate total scores by category, taking into account excused assignments
     for student, grades in gradebook:
-        earned = defaultdict(list)
-        possible = defaultdict(list)
+        earned = {c: [] for c in categories}
+        possible = {c: [] for c in categories}
         for hw in homeworks:
             if max_scores[hw.id] == 0.:
                 continue
@@ -416,6 +418,7 @@ def get_gradebook():
         grades["overall"] = 0.
         for category, poss in possible.iteritems():
             if len(poss) == 0:
+                grades[category.name] = "0 / 0"
                 continue
             # sort scores by benefit to grade if dropped
             e, p = sum(earned[category]), sum(poss)
